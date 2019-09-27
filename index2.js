b5f78f363bc61e90eb3f0e0f8066ac6fcf3fad17
Anthony Shadowitz <anthony.shadowitz@gmail.com>
2019-09-27 01:15:52 +0000

    promises and headaches!

---
+++ index2.js
@@ -0,0 +1,24 @@
+const { fetchMyIP } = require('./iss_promised');
+const { fetchCoordsByIP } = require('./iss_promised');
+const { fetchISSFlyOverTimes } = require('./iss_promised');
+const { nextISSTimesForMyLocation } = require('./iss_promised');
+const { printPassTimes } = require('./iss_promised');
+
+/*
+fetchMyIP()
+.then(fetchCoordsByIP)
+.then(fetchISSFlyOverTimes)
+.then((body) => {
+  const data = JSON.parse(body);
+  console.log(data.response)
+  });
+*/
+
+
+nextISSTimesForMyLocation()
+  .then((passTimes) => {
+    console.log(passTimes);
+  })
+  .catch((error) => {
+    console.log("It didn't work: ", error.message)
+  })
